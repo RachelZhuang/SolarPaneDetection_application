62c1442b6c41e9b830dcca4782b308d13a1658af
lizhuang <923865408@qq.com>
2019-05-24 11:13:02 +0800

    solve python output and exit problem

--- SolarPaneDetection_application/ProgressBar.cs
+++ SolarPaneDetection_application/ProgressBar.cs
@@ -30,69 +30,10 @@ namespace SolarPaneDetection_application
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Thread fThread = new Thread(new ThreadStart(RunPythonScript));
-            fThread.Start();           
+            string exe = @"C:/Users/Administrator/AppData/Local/Programs/Python/Python37/python.exe";
+            string dosCommand = @"C:/Users/Administrator/PycharmProjects/test/test.py";
+            string output = DosCommandOutput.Execute(exe,dosCommand);
         }
-        //调用python核心代码
-        public void RunPythonScript()
-        {
-           
-            Process p = new Process();
-          
-            string path = @"C:/Users/Administrator/PycharmProjects/test/test.py";//(因为我没放debug下，所以直接写的绝对路径,替换掉上面的路径了)
-            p.StartInfo.FileName = @"C:/Users/Administrator/AppData/Local/Programs/Python/Python37/python.exe";//没有配环境变量的话，可以像我这样写python.exe的绝对路径。如果配了，直接写"python.exe"即可
-              
-
-            p.StartInfo.Arguments =path;
-
-            p.StartInfo.UseShellExecute = false;
-
-            p.StartInfo.RedirectStandardOutput = true;
-
-            p.StartInfo.RedirectStandardInput = true;
-
-            p.StartInfo.RedirectStandardError = true;
-
-            p.StartInfo.CreateNoWindow = true;
-
-            p.Start();
-            p.BeginOutputReadLine();
-            p.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
-          
-            //Console.ReadLine();
-            p.WaitForExit();
-            p.Close();
-           
-           
-        }
-        //输出打印的信息
-        public void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(e.Data))
-            {
-                AppendText(e.Data);
-            }
-        }
-        public delegate void AppendTextCallback(string text);
-        public static bool IsNumeric(string value)
-        {
-            return Regex.IsMatch(value, @"^[+-]?\d*[.]?\d*$");
-        }
-        public static int prev_pos = 0, cur_pos = 0;
-
-        public void AppendText(string text)
-        {
-          
-            Console.WriteLine(text);     //此处在控制台输出.py文件print的结果   
-            //从输出文本中提取数字           
-            if (IsNumeric(text))
-            {
-                prev_pos = cur_pos;
-                cur_pos = int.Parse(text);                
-            }
-            text = text + Environment.NewLine;
-            SetTextMesssage(cur_pos,text);
-
-        }
+       
     }
 }
